1f1b80553e0e87fd989a42a14b480b58bc257fe5
oreqizer <oreqizer@gmail.com>
2016-08-13 10:11:50 +0200

    jwt auth

--- config/config.go
+++ config/config.go
@@ -4,8 +4,11 @@ import "flag"
 
 var (
 	Locales = []string{"en-US", "sk"}
+
+	// flaggable:
 	DBurl = *flag.String("db", "postgres://oreqizer@localhost/reactizer", "Database URL")
 	Port = *flag.Int("port", 8080, "Server port")
+	Secret = *flag.String("secret", "wowMuchSecure1337", "Secret")
 )
 
 const (
--- modules/auth.go
+++ modules/auth.go
@@ -3,6 +3,12 @@ package modules
 import (
 	"database/sql"
 	"net/http"
+	"fmt"
+	"log"
+
+	"github.com/dgrijalva/jwt-go"
+
+	"reactizer-go/config"
 )
 
 type AuthError string
@@ -16,10 +22,34 @@ func (e AuthError) Error() string {
 //
 // In case of an error, translation id AuthError is returned.
 func authorize(r *http.Request, db *sql.DB) (int, error) {
-	token := r.Header["X-Authorization"]
-	if len(token) != 1 {
+	data := r.Header["X-Authorization"]
+	if len(data) != 1 {
 		return 0, AuthError("auth.no_auth_header")
 	}
-	
+	token := data[0]
+
+	log.Print(decodeToken(token)) // TODO: create token
 	return 0, nil
+}
+
+func decodeToken(raw string) (int, error) {
+	token, err := jwt.Parse(raw, keyfunc)
+	if err != nil {
+		log.Print(err)
+		return 0, AuthError("auth.invalid_token")
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+    //return claims["sub"].(int), nil
+    return 1, nil
+	}
+
+	return 0, AuthError("auth.invalid_token")
+}
+
+func keyfunc(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Unexpected method: %v", token.Header["alg"])
+	}
+	return config.Secret, nil
 }
