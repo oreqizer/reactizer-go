d6fee8f570523fba2ed2bbc5927a136cbd8b2294
oreqizer <oreqizer@gmail.com>
2016-08-15 17:37:40 +0200

    comments, finished translation gatherer

--- translations.go
+++ translations.go
@@ -8,45 +8,74 @@ import (
 	"regexp"
 	"strings"
 	"encoding/json"
-	"fmt"
+	"sort"
 
 	"github.com/golang/glog"
 )
 
-type message struct {
-	id string
-	translation string
+type Message struct {
+	Id string						`json:"id"`
+	Translation string	`json:"translation"`
 }
 
-func visit(path string, f os.FileInfo, err error) error {
-	if f.IsDir() || strings.HasPrefix(path, ".") || !strings.HasSuffix(path, "errors.go") {
-		return nil
-	}
-	b, err := ioutil.ReadFile(path)
-	if err != nil {
-		return err
-	}
-	r := regexp.MustCompile(`".*"`)
-	translations := r.FindAll(b, -1)
-	messages := []message{}
-	for _, t := range translations {
-		messages = append(messages, message{id: t})
-	}
-	data, err := json.Marshal(messages) // TODO indent
-	if err != nil {
-		return err
-	}
-	fmt.Print(data)
-	return nil
+type Messages []Message
+
+func (m Messages) Len() int {
+	return len(m)
+}
+
+func (m Messages) Less(i, j int) bool {
+	return strings.Compare(m[i].Id, m[j].Id) < 0
+}
+
+func (m Messages) Swap(i, j int) {
+	m[i], m[j] = m[j], m[i]
 }
 
 func main() {
   flag.Parse()
+	messages := Messages{}
+	// visit function
+	visit := func (path string, f os.FileInfo, err error) error {
+		// only walk files named 'errors.go'
+		if f.IsDir() || strings.HasPrefix(path, ".") || !strings.HasSuffix(path, "/errors.go") {
+			return nil
+		}
+		b, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		// take all quoted values
+		r := regexp.MustCompile(`".*"`)
+		translations := r.FindAll(b, -1)
+		for _, t := range translations {
+			// remove redundant quotes
+			id := strings.Replace(string(t), "\"", "", -1)
+			// add to messages
+			messages = append(messages, Message{Id: id})
+		}
+		return nil
+	}
+
   err := filepath.Walk(".", visit)
 	if err != nil {
 		glog.Error(err)
 		return
 	}
-	glog.Info("Success")
+
+	// sort it by id
+	sort.Sort(messages)
+
+	// marshall & write JSON to file
+	data, err := json.MarshalIndent(messages, "", "  ")
+	if err != nil {
+		glog.Error(err)
+		return
+	}
+	err = ioutil.WriteFile("./locales/en-US.untranslated.json", data, os.ModePerm)
+	if err != nil {
+		glog.Error(err)
+		return
+	}
 	return
 }
